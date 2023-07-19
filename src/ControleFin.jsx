9939694f61c252acba38a41a01a6d6b8a7b331a4
robss31 <robss31@hotmail.comgit config --global user.name robsongit config --global user.email robss31@hotmail.com>
2023-07-18 23:45:29 -0300

    atualização da pagina de Ressumo.jsx e ControleFin.jsx

--- src/ControleFin.jsx
+++ src/ControleFin.jsx
@@ -1,19 +1,18 @@
-import react from "react";
+import react, { useState} from "react";
 import './ControleFin.css';
 
 
 
 function ControleFin() {
+
+
     return (
         <div className="container">
             <h1>Controle Financeiro Pessoal</h1>
 
-
-
             <img width={300} height={300} src="./img/foto_capa.jpg" alt="" id="img" />
             <input type="text" id="inputsal" placeholder="Digite Sua Receita Mensal" />
             <button type="submit" id="btn" >Entrar</button>
-
 
         </div>
     )
--- src/Ressumo.jsx
+++ src/Ressumo.jsx
@@ -4,19 +4,39 @@ import './ControleFin.css';
 
 
 function Ressumo() {
+
+
+    const [lista, setLista] = useState([]);
+    const [novo, setNovoItem] = useState("");
+
+    function adicionaItem(form){
+        form.preventDefault();
+        if (!novoItem){
+            return;
+        }
+        setLista([...lista, {text: novoItem, isCompleted: false}]);
+        setNovoItem("");
+        document.getElementById('inputconta').focus();
+    }
+
+
     return (
         <div className="container2">
 
             <h1>Ressumo das Finanças</h1>
 
-            <form >
-                <input id="inputconta" type="text" placeholder="Nome Conta" />
-                <input id="inputconta" type="text" placeholder="Data Venc." />
-                <input id="inputconta" type="text" placeholder="Valor" />
+            <form onSubmit= {adicionaItem}>
+                <input id="inputconta" type="text" value={novoItem} onChange={(e)=> {setNovoItem(e.target.value)}} placeholder="Nome da Conta" />
+                <input id="inputconta" type="text" value={novoItem} placeholder="Data Venc." />
+                <input id="inputconta" type="text" value={novoItem} placeholder="Valor" />
                 <button type="submit" id="btn1">add</button>
             </form>
 
             <div className="listaContas">
+                {
+                    <lista className="length"></lista>
+                }
+
                 <div className="item">
                     <span>Conta de Exemplo</span>
                     <span>Conta de Exemplo1</span>
